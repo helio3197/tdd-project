df517e9816d81a8cb9d7243cea727dbc59f9903c
Kenny <kennysalazar3197@gmail.com>
2022-05-19 16:32:10 -0500

    Create reverse method

--- solver.rb
+++ solver.rb
@@ -1,9 +1,15 @@
 class Solver
   def factorial(int)
-    raise ArgumentError if int.class != Integer
+    raise ArgumentError unless int.is_a?(Integer)
     raise ArgumentError if int.negative?
     return 1 if int.zero?
 
     factorial(int - 1) * int
+  end
+
+  def reverse(str)
+    raise ArgumentError unless str.is_a?(String)
+
+    str.reverse
   end
 end
